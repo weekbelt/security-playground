fe2b119a499f18605b7da6b58fb1c2bb57bb027c
weekbelt <vfrvfr4207@gmail.com>
2022-05-01 17:58:06 +0900

    Initial Commit

---
+++ src/main/java/me/weekbelt/securityplayground/SecurityPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package me.weekbelt.securityplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/weekbelt/securityplayground/SecurityPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weekbelt.securityplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
